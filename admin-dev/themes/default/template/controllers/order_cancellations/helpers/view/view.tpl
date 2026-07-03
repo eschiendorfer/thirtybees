d3e82db4054fa02cffdd763ae7c1ce97be72f40b
eschiendorfer <mandi.schiendorfer@gmail.com>
2026-07-03 08:24:43 +0200

    Adding Order Cancellations Controller

---
+++ admin-dev/themes/default/template/controllers/order_cancellations/helpers/view/view.tpl
@@ -0,0 +1,173 @@
+{extends file="helpers/view/view.tpl"}
+
+{block name="override_tpl"}
+<div class="panel">
+	<h3><i class="icon-ban"></i> {l s='Order cancellation'}</h3>
+	<div class="row form-horizontal">
+		<div class="col-lg-6">
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='ID'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">{$order_cancellation->id|intval}</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Order'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">
+						{if isset($summary.id_order) && $summary.id_order}
+							<a href="{$summary.order_url|escape:'html':'UTF-8'}">{$summary.reference|escape:'html':'UTF-8'}</a>
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Customer'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">
+						{if isset($summary.id_customer) && $summary.id_customer}
+							<a href="{$summary.customer_url|escape:'html':'UTF-8'}">{$summary.customer|escape:'html':'UTF-8'}</a>
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Status'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">{$status_label|escape:'html':'UTF-8'}</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Requested refund method'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">
+						{if $refund_method_label}
+							{$refund_method_label|escape:'html':'UTF-8'}
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Quoted total'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">
+						{if $has_quoted_refund_total}
+							{displayWtPriceWithCurrency price=$order_cancellation->quoted_refund_total_tax_incl currency=$currency}
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Quoted fee'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">
+						{if $has_quoted_fee}
+							{displayWtPriceWithCurrency price=$order_cancellation->quoted_fee_tax_incl currency=$currency}
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Employee'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">
+						{if isset($summary.employee) && $summary.employee}
+							{$summary.employee|escape:'html':'UTF-8'}
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Migrated'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">{if $order_cancellation->migrated}{l s='Yes'}{else}{l s='No'}{/if}</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-lg-4">{l s='Date issued'}</label>
+				<div class="col-lg-8">
+					<p class="form-control-static">{dateFormat date=$order_cancellation->date_add full=true}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="panel">
+	<h3><i class="icon-list"></i> {l s='Products'}</h3>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>{l s='Order detail ID'}</th>
+				<th>{l s='Reference'}</th>
+				<th>{l s='Product'}</th>
+				<th class="text-right">{l s='Ordered quantity'}</th>
+				<th class="text-right">{l s='Cancelled quantity'}</th>
+			</tr>
+		</thead>
+		<tbody>
+			{foreach from=$details item=detail}
+				<tr>
+					<td class="text-center">{$detail.id_order_detail|intval}</td>
+					<td>
+						{if $detail.product_reference}
+							{$detail.product_reference|escape:'html':'UTF-8'}
+						{else}
+							<span class="text-muted">-</span>
+						{/if}
+					</td>
+					<td>{$detail.product_name|escape:'html':'UTF-8'}</td>
+					<td class="text-right">{$detail.ordered_quantity|intval}</td>
+					<td class="text-right">{$detail.cancelled_quantity|intval}</td>
+				</tr>
+			{foreachelse}
+				<tr>
+					<td colspan="5" class="text-center">{l s='No products found.'}</td>
+				</tr>
+			{/foreach}
+		</tbody>
+	</table>
+</div>
+
+<div class="panel">
+	<h3><i class="icon-file-text"></i> {l s='Credit slips'}</h3>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>{l s='Credit slip ID'}</th>
+				<th>{l s='Date issued'}</th>
+				<th class="text-right">{l s='Total products'}</th>
+				<th class="text-right">{l s='Fee'}</th>
+			</tr>
+		</thead>
+		<tbody>
+			{foreach from=$order_slips item=order_slip}
+				<tr>
+					<td><a href="{$order_slip.url|escape:'html':'UTF-8'}">#{$order_slip.id_order_slip|intval}</a></td>
+					<td>{dateFormat date=$order_slip.date_add full=true}</td>
+					<td class="text-right">{displayWtPriceWithCurrency price=$order_slip.total_products_tax_incl currency=$currency}</td>
+					<td class="text-right">{displayWtPriceWithCurrency price=$order_slip.adjustment_fee_tax_incl currency=$currency}</td>
+				</tr>
+			{foreachelse}
+				<tr>
+					<td colspan="4" class="text-center">{l s='No credit slips found.'}</td>
+				</tr>
+			{/foreach}
+		</tbody>
+	</table>
+</div>
+{/block}
---
+++ controllers/admin/AdminOrderCancellationsController.php
@@ -0,0 +1,257 @@
+<?php
+
+/**
+ * Class AdminOrderCancellationsControllerCore
+ *
+ * @property OrderCancellation|null $object
+ */
+class AdminOrderCancellationsControllerCore extends AdminController
+{
+    private const REASON_ENTITY_TYPE_ORDER_CANCELLATION = 71;
+
+    /**
+     * AdminOrderCancellationsControllerCore constructor.
+     *
+     * @throws PrestaShopException
+     */
+    public function __construct()
+    {
+        $this->bootstrap = true;
+        $this->context = Context::getContext();
+        $this->table = 'order_cancellation';
+        $this->className = 'OrderCancellation';
+        $this->identifier = 'id_order_cancellation';
+
+        $this->_select = 'o.`id_order`, o.`id_shop`, o.`id_currency`, o.`reference`, c.`id_customer`, CONCAT(c.`firstname`, \' \', c.`lastname`) AS `customer`, COALESCE(SUM(ocd.`product_quantity`), 0) AS `cancelled_quantity`';
+        $this->_join = ' LEFT JOIN `'._DB_PREFIX_.'orders` o ON (o.`id_order` = a.`id_order`)';
+        $this->_join .= ' LEFT JOIN `'._DB_PREFIX_.'customer` c ON (c.`id_customer` = o.`id_customer`)';
+        $this->_join .= ' LEFT JOIN `'._DB_PREFIX_.'order_cancellation_detail` ocd ON (ocd.`id_order_cancellation` = a.`id_order_cancellation`)';
+        $this->_group = ' GROUP BY a.`id_order_cancellation`';
+
+        $this->fields_list = [
+            'id_order_cancellation' => [
+                'title' => $this->l('ID'),
+                'align' => 'text-center',
+                'class' => 'fixed-width-xs',
+            ],
+            'reference' => [
+                'title' => $this->l('Order Reference'),
+                'filter_key' => 'o!reference',
+                'align' => 'text-center',
+                'class' => 'fixed-width-xs',
+            ],
+            'customer' => [
+                'title' => $this->l('Customer'),
+                'havingFilter' => true,
+            ],
+            'status' => [
+                'title' => $this->l('Status'),
+                'filter_key' => 'a!status',
+            ],
+            'requested_refund_method' => [
+                'title' => $this->l('Requested refund method'),
+                'filter_key' => 'a!requested_refund_method',
+            ],
+            'cancelled_quantity' => [
+                'title' => $this->l('Cancelled quantity'),
+                'align' => 'text-center',
+                'havingFilter' => true,
+                'class' => 'fixed-width-sm',
+            ],
+            'date_add' => [
+                'title' => $this->l('Date issued'),
+                'type' => 'datetime',
+                'align' => 'right',
+                'filter_key' => 'a!date_add',
+            ],
+        ];
+
+        $this->_orderBy = 'id_order_cancellation';
+        $this->_orderWay = 'DESC';
+        $this->_use_found_rows = false;
+
+        parent::__construct();
+
+        $this->_where = Shop::addSqlRestriction(false, 'o');
+        $this->addRowAction('view');
+    }
+
+    /**
+     * @return void
+     *
+     * @throws PrestaShopException
+     */
+    public function initToolbar()
+    {
+        parent::initToolbar();
+
+        unset($this->toolbar_btn['new']);
+    }
+
+    /**
+     * @return string
+     *
+     * @throws PrestaShopDatabaseException
+     * @throws PrestaShopException
+     * @throws SmartyException
+     */
+    public function renderView()
+    {
+        if (!Validate::isLoadedObject($this->object)) {
+            return '';
+        }
+
+        $summary = $this->getCancellationSummary((int)$this->object->id);
+        $details = $this->getCancellationDetails((int)$this->object->id);
+        $orderSlips = $this->getCancellationOrderSlips((int)$this->object->id);
+        $currency = $this->context->currency;
+
+        if ($summary) {
+            if ((int)$summary['id_currency'] > 0) {
+                $orderCurrency = new Currency((int)$summary['id_currency']);
+                if (Validate::isLoadedObject($orderCurrency)) {
+                    $currency = $orderCurrency;
+                }
+            }
+
+            $summary['order_url'] = $this->context->link->getAdminLink('AdminOrders', true, [
+                'vieworder' => true,
+                'id_order' => (int)$summary['id_order'],
+            ]);
+            $summary['customer_url'] = $this->context->link->getAdminLink('AdminCustomers', true, [
+                'viewcustomer' => true,
+                'id_customer' => (int)$summary['id_customer'],
+            ]);
+        }
+
+        foreach ($orderSlips as &$orderSlip) {
+            $orderSlip['url'] = $this->context->link->getAdminLink(
+                'AdminSlip',
+                true,
+                [],
+                ['id_order_slip' => (int)$orderSlip['id_order_slip']]
+            );
+        }
+        unset($orderSlip);
+
+        $this->tpl_view_vars = [
+            'order_cancellation' => $this->object,
+            'summary' => $summary,
+            'details' => $details,
+            'order_slips' => $orderSlips,
+            'status_label' => $this->getStatusLabel((string)$this->object->status),
+            'refund_method_label' => $this->getRefundMethodLabel((string)$this->object->requested_refund_method),
+            'currency' => $currency,
+            'has_quoted_refund_total' => $this->object->quoted_refund_total_tax_incl !== null && $this->object->quoted_refund_total_tax_incl !== '',
+            'has_quoted_fee' => $this->object->quoted_fee_tax_incl !== null && $this->object->quoted_fee_tax_incl !== '',
+        ];
+
+        return parent::renderView();
+    }
+
+    /**
+     * @return array
+     */
+    private function getStatusList(): array
+    {
+        return [
+            OrderCancellation::STATUS_REQUESTED => $this->l('Requested'),
+            OrderCancellation::STATUS_APPLIED => $this->l('Applied'),
+            OrderCancellation::STATUS_REJECTED => $this->l('Rejected'),
+            OrderCancellation::STATUS_EXPIRED => $this->l('Expired'),
+        ];
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return string
+     */
+    private function getStatusLabel(string $status): string
+    {
+        return $this->getStatusList()[$status] ?? $status;
+    }
+
+    /**
+     * @param string $refundMethod
+     *
+     * @return string
+     */
+    private function getRefundMethodLabel(string $refundMethod): string
+    {
+        if ($refundMethod === '') {
+            return '';
+        }
+
+        $labels = [
+            OrderCancellation::REFUND_METHOD_STORE_CREDIT => $this->l('Store credit'),
+            OrderCancellation::REFUND_METHOD_ORIGINAL_PAYMENT => $this->l('Original payment'),
+        ];
+
+        return $labels[$refundMethod] ?? str_replace('_', ' ', $refundMethod);
+    }
+
+    /**
+     * @param int $idOrderCancellation
+     *
+     * @return array|null
+     *
+     * @throws PrestaShopDatabaseException
+     * @throws PrestaShopException
+     */
+    private function getCancellationSummary(int $idOrderCancellation): ?array
+    {
+        $row = Db::getInstance()->getRow(
+            (new DbQuery())
+                ->select('o.`id_order`, o.`id_currency`, o.`reference`, c.`id_customer`, CONCAT(c.`firstname`, \' \', c.`lastname`) AS `customer`, CONCAT(e.`firstname`, \' \', e.`lastname`) AS `employee`')
+                ->from('order_cancellation', 'oc')
+                ->leftJoin('orders', 'o', 'o.`id_order` = oc.`id_order`')
+                ->leftJoin('customer', 'c', 'c.`id_customer` = o.`id_customer`')
+                ->leftJoin('employee', 'e', 'e.`id_employee` = oc.`id_employee`')
+                ->where('oc.`id_order_cancellation` = '.(int)$idOrderCancellation)
+        );
+
+        return $row ?: null;
+    }
+
+    /**
+     * @param int $idOrderCancellation
+     *
+     * @return array
+     *
+     * @throws PrestaShopDatabaseException
+     * @throws PrestaShopException
+     */
+    private function getCancellationDetails(int $idOrderCancellation): array
+    {
+        return Db::getInstance()->getArray(
+            (new DbQuery())
+                ->select('ocd.`id_order_detail`, od.`product_reference`, od.`product_name`, od.`product_quantity` AS `ordered_quantity`, ocd.`product_quantity` AS `cancelled_quantity`')
+                ->from('order_cancellation_detail', 'ocd')
+                ->leftJoin('order_detail', 'od', 'od.`id_order_detail` = ocd.`id_order_detail`')
+                ->where('ocd.`id_order_cancellation` = '.(int)$idOrderCancellation)
+                ->orderBy('ocd.`id_order_cancellation_detail` ASC')
+        );
+    }
+
+    /**
+     * @param int $idOrderCancellation
+     *
+     * @return array
+     *
+     * @throws PrestaShopDatabaseException
+     * @throws PrestaShopException
+     */
+    private function getCancellationOrderSlips(int $idOrderCancellation): array
+    {
+        return Db::getInstance()->getArray(
+            (new DbQuery())
+                ->select('os.`id_order_slip`, os.`date_add`, os.`total_products_tax_incl`, os.`adjustment_fee_tax_incl`')
+                ->from('order_slip', 'os')
+                ->where('os.`reason_entity_type` = '.self::REASON_ENTITY_TYPE_ORDER_CANCELLATION)
+                ->where('os.`reason_id_entity` = '.(int)$idOrderCancellation)
+                ->orderBy('os.`id_order_slip` ASC')
+        );
+    }
+
+}
